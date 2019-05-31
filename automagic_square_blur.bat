5ff67fca66c84882d162a3d3396feadfe9842a0f
Eric Coutinho <ericcdsoouza@gmail.com>
2019-05-31 18:16:08 -0300

    Add files via upload

---
+++ automagic_square_blur.bat
@@ -0,0 +1,254 @@
+@echo off
+color 0a
+echo.
+echo ===========================================================================================
+echo.
+echo ********************************** Automagic Square Blur **********************************
+echo.
+echo ===========================================================================================
+echo --------------------------------------------------------------------------------------------
+echo Written by: Eric Coutinho								
+echo github.com/ericcoutinho									
+echo Description: Automagically resize your images with a Square Blur background whitout crop
+echo Version: 05/31/2019	
+echo Licences: imagemagick.org						
+echo --------------------------------------------------------------------------------------------
+echo.
+echo.
+echo -----------------------------------Instructions to use-------------------------------------
+echo.
+echo .                          Follow this instructions before use                            .
+echo.
+echo 1) Put all files from this program in the image folder you want to convert
+echo 2) Download ImageMagick at imagemagick.org website and install
+echo 3) Execute this program and automagically resize your images with a Square Blur background whitout crop
+echo.
+echo .                    All images from your folder will be converted                        . 
+echo .               Leave in the folder only the images you want to convert                   .
+echo .                          Images name can not have spaces                                .
+echo .  Each image dimension (width x height) will be base on the height value of original image  .
+echo.
+pause
+echo  ============================ here is where the magic happens =============================
+echo magick "%%I" ^( -clone 0 -blur 0x9 -resize %%hx%%h^! ^) ^ -resize %%hx%%h +swap
+echo -gravity center -compose over -composite %%I
+echo.
+echo ===========================================================================================
+echo  ..........................................Loading..........................................      
+echo --------------------------------------------------------------------------------------------
+echo .                         [ ][ ][ ][ ][ ][ ][ ][ ][ ][ ] =   0 ]                           .
+echo --------------------------------------------------------------------------------------------
+ping localhost -n 1 >nul
+cls
+echo ===========================================================================================
+echo.
+echo ********************************** Automagic Square Blur **********************************
+echo.
+echo ===========================================================================================
+echo --------------------------------------------------------------------------------------------
+echo Written by: Eric Coutinho								
+echo github.com/ericcoutinho									
+echo Description: Automagically resize your images with a Square Blur background whitout crop
+echo Version: 05/31/2019	
+echo Licences: imagemagick.org						
+echo --------------------------------------------------------------------------------------------
+echo.
+echo.
+echo  ============================ here is where the magic happens =============================
+echo.
+echo magick "%%I" ^( -clone 0 -blur 0x9 -resize %%hx%%h^! ^) ^ -resize %%hx%%h +swap
+echo -gravity center -compose over -composite %%I
+echo.
+echo ===========================================================================================
+echo  ..........................................Loading..........................................      
+echo --------------------------------------------------------------------------------------------
+echo .                          [*][*][ ][ ][ ][ ][ ][ ][ ][ ] =   25 ]                         .
+echo --------------------------------------------------------------------------------------------
+ping localhost -n 1 >nul
+cls
+echo ===========================================================================================
+echo.
+echo ********************************** Automagic Square Blur **********************************
+echo.
+echo ===========================================================================================
+echo --------------------------------------------------------------------------------------------
+echo Written by: Eric Coutinho								
+echo github.com/ericcoutinho									
+echo Description: Automagically resize your images with a Square Blur background whitout crop
+echo Version: 05/31/2019	
+echo Licences: imagemagick.org						
+echo --------------------------------------------------------------------------------------------
+echo.
+echo.
+echo  ============================ here is where the magic happens =============================
+echo.
+echo magick "%%I" ^( -clone 0 -blur 0x9 -resize %%hx%%h^! ^) ^ -resize %%hx%%h +swap
+echo -gravity center -compose over -composite %%I
+echo.
+echo ===========================================================================================
+echo  ..........................................Loading..........................................
+echo --------------------------------------------------------------------------------------------
+echo  .                         [*][*][*][ ][ ][ ][ ][ ][ ][ ] =   35 ]                         .
+echo --------------------------------------------------------------------------------------------
+ping localhost -n 1 >nul
+cls
+echo ===========================================================================================
+echo.
+echo ********************************** Automagic Square Blur **********************************
+echo.
+echo ===========================================================================================
+echo --------------------------------------------------------------------------------------------
+echo Written by: Eric Coutinho								
+echo github.com/ericcoutinho									
+echo Description: Automagically resize your images with a Square Blur background whitout crop
+echo Version: 05/31/2019	
+echo Licences: imagemagick.org						
+echo --------------------------------------------------------------------------------------------
+echo.
+echo.
+echo  ============================ here is where the magic happens =============================
+echo.
+echo magick "%%I" ^( -clone 0 -blur 0x9 -resize %%hx%%h^! ^) ^ -resize %%hx%%h +swap
+echo -gravity center -compose over -composite %%I
+echo.
+echo ===========================================================================================
+echo  ..........................................Loading..........................................
+echo --------------------------------------------------------------------------------------------
+echo  .                        [*][*][*][*][*][ ][ ][ ][ ][ ] =   50 ]                          .
+echo --------------------------------------------------------------------------------------------
+ping localhost -n 1 >nul
+cls
+echo ===========================================================================================
+echo.
+echo ********************************** Automagic Square Blur **********************************
+echo.
+echo ===========================================================================================
+echo --------------------------------------------------------------------------------------------
+echo Written by: Eric Coutinho								
+echo github.com/ericcoutinho									
+echo Description: Automagically resize your images with a Square Blur background whitout crop
+echo Version: 05/31/2019	
+echo Licences: imagemagick.org						
+echo --------------------------------------------------------------------------------------------
+echo.
+echo.
+echo  ============================ here is where the magic happens =============================
+echo.
+echo magick "%%I" ^( -clone 0 -blur 0x9 -resize %%hx%%h^! ^) ^ -resize %%hx%%h +swap
+echo -gravity center -compose over -composite %%I
+echo.
+echo ===========================================================================================
+echo    ..........................................Loading..........................................
+echo --------------------------------------------------------------------------------------------
+echo  .                        [*][*][*][*][*][*][*][ ][ ][ ] =   67 ]                          .
+echo --------------------------------------------------------------------------------------------
+ping localhost -n 1 >nul
+cls
+echo ===========================================================================================
+echo.
+echo ********************************** Automagic Square Blur **********************************
+echo.
+echo ===========================================================================================
+echo --------------------------------------------------------------------------------------------
+echo Written by: Eric Coutinho								
+echo github.com/ericcoutinho									
+echo Description: Automagically resize your images with a Square Blur background whitout crop
+echo Version: 05/31/2019	
+echo Licences: imagemagick.org						
+echo --------------------------------------------------------------------------------------------
+echo.
+echo.
+echo  ============================ here is where the magic happens =============================
+echo.
+echo magick "%%I" ^( -clone 0 -blur 0x9 -resize %%hx%%h^! ^) ^ -resize %%hx%%h +swap
+echo -gravity center -compose over -composite %%I
+echo.
+echo ===========================================================================================
+echo  ..........................................Loading..........................................
+echo --------------------------------------------------------------------------------------------
+echo .                         [*][*][*][*][*][*][*][*][ ][ ] =   75 ]                          .
+echo --------------------------------------------------------------------------------------------
+ping localhost -n 1 >nul
+cls
+echo ===========================================================================================
+echo.
+echo ********************************** Automagic Square Blur **********************************
+echo.
+echo ===========================================================================================
+echo --------------------------------------------------------------------------------------------
+echo Written by: Eric Coutinho								
+echo github.com/ericcoutinho									
+echo Description: Automagically resize your images with a Square Blur background whitout crop
+echo Version: 05/31/2019	
+echo Licences: imagemagick.org						
+echo --------------------------------------------------------------------------------------------
+echo.
+echo.
+echo  ============================ here is where the magic happens =============================
+echo.
+echo magick "%%I" ^( -clone 0 -blur 0x9 -resize %%hx%%h^! ^) ^ -resize %%hx%%h +swap
+echo -gravity center -compose over -composite %%I
+echo.
+echo ===========================================================================================
+echo  ..........................................Loading..........................................
+echo ---------------------------------------
+echo .                          [*][*][*][*][*][*][*][*][*][ ] =   89 ]                        .
+echo --------------------------------------------------------------------------------------------
+ping localhost -n 1 >nul
+cls
+echo ===========================================================================================
+echo.
+echo ********************************** Automagic Square Blur **********************************
+echo.
+echo ===========================================================================================
+echo --------------------------------------------------------------------------------------------
+echo Written by: Eric Coutinho								
+echo github.com/ericcoutinho									
+echo Description: Automagically resize your images with a Square Blur background whitout crop
+echo Version: 05/31/2019	
+echo Licences: imagemagick.org						
+echo --------------------------------------------------------------------------------------------
+echo.
+echo.
+echo  ============================ here is where the magic happens =============================
+echo.
+echo magick "%%I" ^( -clone 0 -blur 0x9 -resize %%hx%%h^! ^) ^ -resize %%hx%%h +swap
+echo -gravity center -compose over -composite %%I
+echo.
+echo ===========================================================================================
+echo  ..........................................Loading..........................................
+echo -------------------------------------------------------------------------------------------
+echo .                       [*][*][*][*][*][*][*][*][*][*] =   100 ]                          .
+echo -------------------------------------------------------------------------------------------
+ping localhost -n 2 >nul
+for %%I in ( * ) do magick "%%I"[0] ^( -clone 0 -blur 0x9 -resize %%hx%%h^! ^) ^ -resize %%hx%%h +swap -gravity center -compose over -composite %%I
+cls
+echo ===========================================================================================
+echo.
+echo ********************************** Automagic Square Blur **********************************
+echo.
+echo ===========================================================================================
+echo -------------------------------------------------------------------------------------------
+echo Written by: Eric Coutinho								
+echo github.com/ericcoutinho									
+echo Description: Automagically resize your images with a Square Blur background whitout crop
+echo Version: 05/31/2019	
+echo Licences: imagemagick.org						
+echo -------------------------------------------------------------------------------------------
+echo.
+echo.
+echo  ============================ here is where the magic happens =============================
+echo.
+echo magick "%%I" ^( -clone 0 -blur 0x9 -resize %%hx%%h^! ^) ^ -resize %%hx%%h +swap
+echo -gravity center -compose over -composite %%I
+echo.
+echo ===========================================================================================
+echo ..........................................Loading..........................................
+echo -------------------------------------------------------------------------------------------
+echo .                       [*][*][*][*][*][*][*][*][*][*] =   100 ]                          .
+echo -------------------------------------------------------------------------------------------
+echo.
+echo .....................................Process Completed..................................... 
+echo.
+echo ...............................Please open your folder image...............................
+pause >nul
